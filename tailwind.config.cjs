3f0f1088daec724400552013515826de4c86abcc
jaggerharness <jaggerharness5@gmail.com>
2023-02-28 22:14:01 -0600

    Begin UI implementation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ws-deep-purple': '#0C0032',
+        'ws-purple': '#190061',
+        'ws-deep-blue': '#240090',
+        'ws-blue': '#3500D3',
+        'ws-black': '#282828',
+      }
+    },
   },
   plugins: [],
 }
