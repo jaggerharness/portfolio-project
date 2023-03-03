526f5e7ac438593d1d8074175f3498116bfad08c
Jagger <jagger@aceonetechnologies.com>
2023-03-02 21:21:07 -0600

    UI updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <div class="bg-ws-black flex h-screen justify-center items-center">
-        <div class="bg-ws-gray text-white text-xl font-mono p-10 rounded-md border border-ws-red shadow-lg shadow-ws-crimson">
+        <div class="bg-ws-gray text-white text-xl font-mono p-10 rounded-md border border-ws-crimson shadow-lg shadow-ws-red">
           Hello, World!
         </div>
       </div>
